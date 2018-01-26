82cf89e8c1d5720dc92f00445a47bc3daa0ac865
andrust <tamas.andrus@gmail.com>
2018-01-26 22:06:08 +0100

    Telemetry has thread

--- rpi/Telemetry.cc
+++ rpi/Telemetry.cc
@@ -1,7 +1,31 @@
 #include "Telemetry.h"
 
+constexpr std::chrono::milliseconds Telemetry::DEFAULT_TELEMETRY_INTERVAL;
+
+Telemetry::Telemetry() {
+    _senderThread = std::thread(std::bind(&Telemetry::senderThread, this));
+}
+
+Telemetry::~Telemetry() {
+    _stopFlag = true;
+    if (_senderThread.joinable() ) {
+        _senderThread.join();
+    }
+}
+
 void Telemetry::updateOrientation(uint32_t pitch, uint32_t roll, uint32_t yaw) {
+    _pitch = pitch;
+    _roll = roll;
+    _yaw = yaw;
 }
 
 void Telemetry::updateTime(const std::chrono::system_clock::time_point& now) {
+    using namespace std::chrono;
+    _ts = duration_cast<microseconds>(now.time_since_epoch()).count();
+}
+
+void Telemetry::senderThread() {
+    while (!_stopFlag) {
+        std::this_thread::sleep_for(DEFAULT_TELEMETRY_INTERVAL);
+    }
 }
--- rpi/Telemetry.h
+++ rpi/Telemetry.h
@@ -2,12 +2,29 @@
 
 #include <cstdint>
 #include <chrono>
+#include <atomic>
+#include <thread>
 
 class Telemetry {
 public:
-    explicit Telemetry() = default;
+    Telemetry();
+    ~Telemetry();
+
+private:
+    static constexpr std::chrono::milliseconds DEFAULT_TELEMETRY_INTERVAL{200};
 
 public:
     void updateOrientation(uint32_t pitch, uint32_t roll, uint32_t yaw);
     void updateTime(const std::chrono::system_clock::time_point& now);
+
+private:
+    void senderThread();
+
+private:
+    bool _stopFlag;
+    std::thread _senderThread;
+    std::atomic_uint _pitch;
+    std::atomic_uint _roll;
+    std::atomic_uint _yaw;
+    std::atomic_uint _ts;
 };
